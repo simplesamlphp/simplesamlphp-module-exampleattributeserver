4c7d73a082286a7eb4e426d83a1179b1021b4a90
Tim van Dijen <tvdijen@gmail.com>
2021-01-16 17:50:42 +0100

    Migrate to controller

---
+++ lib/Controller/AttributeServer.php
@@ -0,0 +1,160 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Module\exampleattributeserver\Controller;
+
+use SAML2\Assertion;
+use SAML2\AttributeQuery;
+use SAML2\Binding;
+use SAML2\Constants;
+use SAML2\HTTPPost;
+use SAML2\Response;
+use SAML2\XML\saml\Issuer;
+use SAML2\XML\saml\SubjectConfirmation;
+use SAML2\XML\saml\SubjectConfirmationData;
+use SimpleSAML\Configuration;
+use SimpleSAML\Error;
+use SimpleSAML\HTTP\RunnableResponse;
+use SimpleSAML\Logger;
+use SimpleSAML\Session;
+use SimpleSAML\Metadata\MetaDataStorageHandler;
+use SimpleSAML\Module\saml\Message;
+use SimpleSAML\XHTML\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Controller class for the exampleattributeserver module.
+ *
+ * This class serves the attribute server available in the module.
+ *
+ * @package SimpleSAML\Module\exampleattributeserver
+ */
+class AttributeServer
+{
+    /** @var \SimpleSAML\Configuration */
+    protected $config;
+
+    /** @var \SimpleSAML\Session */
+    protected $session;
+
+
+    /**
+     * ConfigController constructor.
+     *
+     * @param \SimpleSAML\Configuration $config The configuration to use.
+     * @param \SimpleSAML\Session $session The current user session.
+     */
+    public function __construct(Configuration $config, Session $session)
+    {
+        $this->config = $config;
+        $this->session = $session;
+    }
+
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request The current request.
+     *
+     * @return \SimpleSAML\HTTP\RunnableResponse
+     * @throws \SimpleSAML\Error\BadRequest
+     */
+    public function main(/** @scrutinizer ignore-unused */ Request $request): RunnableResponse
+    {
+        $metadata = MetaDataStorageHandler::getMetadataHandler();
+
+        $binding = Binding::getCurrentBinding();
+        $query = $binding->receive();
+        if (!($query instanceof AttributeQuery)) {
+            throw new Error\BadRequest('Invalid message received to AttributeQuery endpoint.');
+        }
+
+        $idpEntityId = $metadata->getMetaDataCurrentEntityID('saml20-idp-hosted');
+
+        $issuer = $query->getIssuer();
+        if ($issuer === null) {
+            throw new Error\BadRequest('Missing <saml:Issuer> in <samlp:AttributeQuery>.');
+        } else {
+            $spEntityId = $issuer->getValue();
+            if ($spEntityId === '') {
+                throw new Error\BadRequest('Empty <saml:Issuer> in <samlp:AttributeQuery>.');
+            }
+        }
+
+        $idpMetadata = $metadata->getMetaDataConfig($idpEntityId, 'saml20-idp-hosted');
+        $spMetadata = $metadata->getMetaDataConfig($spEntityId, 'saml20-sp-remote');
+
+        // The endpoint we should deliver the message to
+        $endpoint = $spMetadata->getString('testAttributeEndpoint');
+
+        // The attributes we will return
+        $attributes = [
+            'name' => ['value1', 'value2', 'value3'],
+            'test' => ['test'],
+        ];
+
+        // The name format of the attributes
+        $attributeNameFormat = Constants::NAMEFORMAT_UNSPECIFIED;
+
+        // Determine which attributes we will return
+        $returnAttributes = array_keys($query->getAttributes());
+        if (count($returnAttributes) === 0) {
+            Logger::debug('No attributes requested - return all attributes.');
+            $returnAttributes = $attributes;
+        } elseif ($query->getAttributeNameFormat() !== $attributeNameFormat) {
+            Logger::debug('Requested attributes with wrong NameFormat - no attributes returned.');
+            $returnAttributes = [];
+        } else {
+            /** @var array $values */
+            foreach ($returnAttributes as $name => $values) {
+                if (!array_key_exists($name, $attributes)) {
+                    // We don't have this attribute
+                    unset($returnAttributes[$name]);
+                    continue;
+                }
+                if (count($values) === 0) {
+                    // Return all attributes
+                    $returnAttributes[$name] = $attributes[$name];
+                    continue;
+                }
+
+                // Filter which attribute values we should return
+                $returnAttributes[$name] = array_intersect($values, $attributes[$name]);
+            }
+        }
+
+        // $returnAttributes contains the attributes we should return. Send them
+        $issuer = new Issuer();
+        $issuer->setValue($idpEntityId);
+
+        $assertion = new Assertion();
+        $assertion->setIssuer($issuer);
+        $assertion->setNameId($query->getNameId());
+        $assertion->setNotBefore(time());
+        $assertion->setNotOnOrAfter(time() + 300); // 60*5 = 5min
+        $assertion->setValidAudiences([$spEntityId]);
+        $assertion->setAttributes($returnAttributes);
+        $assertion->setAttributeNameFormat($attributeNameFormat);
+
+        $sc = new SubjectConfirmation();
+        $sc->setMethod(Constants::CM_BEARER);
+
+        $scd = new SubjectConfirmationData();
+        $scd->setNotOnOrAfter(time() + 300); // 60*5 = 5min
+        $scd->setRecipient($endpoint);
+        $scd->setInResponseTo($query->getId());
+        $sc->setSubjectConfirmationData($scd);
+        $assertion->setSubjectConfirmation([$sc]);
+
+        Message::addSign($idpMetadata, $spMetadata, $assertion);
+
+        $response = new Response();
+        $response->setRelayState($query->getRelayState());
+        $response->setDestination($endpoint);
+        $response->setIssuer($issuer);
+        $response->setInResponseTo($query->getId());
+        $response->setAssertions([$assertion]);
+        Message::addSign($idpMetadata, $spMetadata, $response);
+
+        return new RunnableResponse([new HTTPPost(), 'send'], [$response]);
+    }
+}
---
+++ tests/lib/Controller/AttributeServerTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\Module\exampleattributeserver\Controller;
+
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Configuration;
+use SimpleSAML\HTTP\RunnableResponse;
+use SimpleSAML\Module\exampleattributeserver\Controller;
+use SimpleSAML\Session;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Set of tests for the controllers in the "exampleattributeserver" module.
+ *
+ * @covers \SimpleSAML\Module\exampleattributeserver\Controller\AttributeServer
+ * @package SimpleSAML\Test
+ */
+class ExampleAttributeServerTest extends TestCase
+{
+    /** @var \SimpleSAML\Configuration */
+    protected $config;
+
+    /** @var \SimpleSAML\Session */
+    protected $session;
+
+
+    /**
+     * Set up for each test.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_GET = ['SAMLRequest' => 'rZNRa8IwFIXfB%2FsPIe9Nm7SaNWhBEEFwg%2BHYw95ie%2Bsy2rQkKei%2FX9o6kQ1EhoQQOLnn3I8bMrOyrlqxcM6oXefgtQNzRIe60lYMV3PcGS0aaZUVWtZghcvFdvG8EYxEojWNa%2FKmwheW6w5pLRinGo3RejnHUpYspuk0oJzHAaM0DhKeyKAESpOEMrnjDKN3MNZb5tgneJ%2B1Hay1dVI7L0WUB1EaRNM3SkWSCsY%2FcPb4gNCspxFDtUGrxtTSXWfrFVUE5VAqQDvljjj7dK61IgzhIOu2AtKYfTgLL7Ivmm273RfkblB%2BtBeful7%2BA6DTtoVclQoKnPW2E4GwY5cTxJg%2FQoR%2FKEaG8%2FuivvoEoQpBSUymhJLE70nKYn%2F0i%2BOh7hZk6ZN79QxtFEYro0AX1XFsNgyygtqfFoc3UDEy8UTJXSGsvrE1nZKnJOrHQGMeR6SfSZzelaU1UIIxUGyk3ndyDwPa%2BHi%2F%2F2L2DQ%3D%3D'];
+        $_GET['RelayState'] = 'something';
+        $_SERVER['QUERY_STRING'] = 'SAMLRequest=rZNRa8IwFIXfB%2FsPIe9Nm7SaNWhBEEFwg%2BHYw95ie%2Bsy2rQkKei%2FX9o6kQ1EhoQQOLnn3I8bMrOyrlqxcM6oXefgtQNzRIe60lYMV3PcGS0aaZUVWtZghcvFdvG8EYxEojWNa%2FKmwheW6w5pLRinGo3RejnHUpYspuk0oJzHAaM0DhKeyKAESpOEMrnjDKN3MNZb5tgneJ%2B1Hay1dVI7L0WUB1EaRNM3SkWSCsY%2FcPb4gNCspxFDtUGrxtTSXWfrFVUE5VAqQDvljjj7dK61IgzhIOu2AtKYfTgLL7Ivmm273RfkblB%2BtBeful7%2BA6DTtoVclQoKnPW2E4GwY5cTxJg%2FQoR%2FKEaG8%2FuivvoEoQpBSUymhJLE70nKYn%2F0i%2BOh7hZk6ZN79QxtFEYro0AX1XFsNgyygtqfFoc3UDEy8UTJXSGsvrE1nZKnJOrHQGMeR6SfSZzelaU1UIIxUGyk3ndyDwPa%2BHi%2F%2F2L2DQ%3D%3D&RelayState=something';
+
+        $this->config = Configuration::loadFromArray(
+            [
+                'module.enable' => ['exampleattributeserver' => true],
+            ],
+            '[ARRAY]',
+            'simplesaml'
+        );
+
+        $this->session = Session::getSessionFromRequest();
+/*
+        Configuration::setPreLoadedConfig(
+            Configuration::loadFromArray(
+                [
+                    'admin' => ['core:AdminPassword'],
+                ],
+                '[ARRAY]',
+                'simplesaml'
+            ),
+            'authsources.php',
+            'simplesaml'
+        );
+*/
+    }
+
+
+    /**
+     */
+    public function testMain(): void
+    {
+        $_SERVER['REQUEST_URI'] = '/module.php/exampleattributeserver/attributeserver';
+        $request = Request::create(
+            '/module.php/exampleattributeserver/attributeserver',
+            'GET'
+        );
+
+        $c = new Controller\AttributeServer($this->config, $this->session);
+        $response = $c->main($request);
+
+        $this->assertInstanceOf(RunnableResponse::class, $response);
+        $this->assertTrue($response->isSuccessful());
+    }
+}
