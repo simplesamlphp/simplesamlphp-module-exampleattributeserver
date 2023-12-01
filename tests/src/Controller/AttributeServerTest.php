c83aa3538eac88465cb84fdc7c4da65debd99b64
Tim van Dijen <tvdijen@gmail.com>
2023-12-01 21:31:13 +0100

    Bump test-framework

--- tests/src/Controller/AttributeServerTest.php
+++ tests/src/Controller/AttributeServerTest.php
@@ -9,7 +9,6 @@ use SimpleSAML\Configuration;
 use SimpleSAML\HTTP\RunnableResponse;
 use SimpleSAML\Metadata\MetaDataStorageHandler;
 use SimpleSAML\Module\exampleattributeserver\Controller;
-use SimpleSAML\Session;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -21,18 +20,15 @@ use Symfony\Component\HttpFoundation\Request;
 class ExampleAttributeServerTest extends TestCase
 {
     /** @var \SimpleSAML\Configuration */
-    protected Configuration $config;
-
-    /** @var \SimpleSAML\Session */
-    protected Session $session;
+    protected static Configuration $config;
 
 
     /**
      * Set up for each test.
      */
-    protected function setUp(): void
+    public static function setUpBeforeClass(): void
     {
-        parent::setUp();
+        parent::setUpBeforeClass();
 
         $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
         $_SERVER['REQUEST_METHOD'] = 'GET';
@@ -40,15 +36,13 @@ class ExampleAttributeServerTest extends TestCase
         $_GET['RelayState'] = 'something';
         $_SERVER['QUERY_STRING'] = 'SAMLRequest=rZNRa8IwFIXfB%2FsPIe9Nm7SaNWhBEEFwg%2BHYw95ie%2Bsy2rQkKei%2FX9o6kQ1EhoQQOLnn3I8bMrOyrlqxcM6oXefgtQNzRIe60lYMV3PcGS0aaZUVWtZghcvFdvG8EYxEojWNa%2FKmwheW6w5pLRinGo3RejnHUpYspuk0oJzHAaM0DhKeyKAESpOEMrnjDKN3MNZb5tgneJ%2B1Hay1dVI7L0WUB1EaRNM3SkWSCsY%2FcPb4gNCspxFDtUGrxtTSXWfrFVUE5VAqQDvljjj7dK61IgzhIOu2AtKYfTgLL7Ivmm273RfkblB%2BtBeful7%2BA6DTtoVclQoKnPW2E4GwY5cTxJg%2FQoR%2FKEaG8%2FuivvoEoQpBSUymhJLE70nKYn%2F0i%2BOh7hZk6ZN79QxtFEYro0AX1XFsNgyygtqfFoc3UDEy8UTJXSGsvrE1nZKnJOrHQGMeR6SfSZzelaU1UIIxUGyk3ndyDwPa%2BHi%2F%2F2L2DQ%3D%3D&RelayState=something';
 
-        $this->config = Configuration::loadFromArray(
+        self::$config = Configuration::loadFromArray(
             [
                 'module.enable' => ['exampleattributeserver' => true],
             ],
             '[ARRAY]',
             'simplesaml'
         );
-
-        $this->session = Session::getSessionFromRequest();
     }
 
 
@@ -67,7 +61,8 @@ class ExampleAttributeServerTest extends TestCase
         $mdh->method('getMetaDataConfig')->willReturn(Configuration::loadFromArray([
             'EntityID' => 'auth_source_id',
             'testAttributeEndpoint' => 'test',
-            'privatekey' => '../../../../vendor/simplesamlphp/xml-security/tests/resources/certificates/selfsigned.simplesamlphp.org.key',
+            'privatekey' => dirname(__FILE__, 4) . '/vendor/simplesamlphp/xml-security/resources/keys/selfsigned.simplesamlphp.org.key',
+            'privatekey_pass' => '1234',
         ]));
 /**
         $mdh = new class () extends MetaDataStorageHandler {
@@ -103,7 +98,7 @@ class ExampleAttributeServerTest extends TestCase
         };
 */
 
-        $c = new Controller\AttributeServer($this->config, $this->session);
+        $c = new Controller\AttributeServer(self::$config);
         $c->setMetadataStorageHandler($mdh);
         $response = $c->main($request);
 
